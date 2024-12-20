576857aea08d9c1da8185e0355aad03bcbf2d8b9
jameson <jamesonmacarthur@gmail.com>
2024-12-20 14:43:56 -0700

    adds starfield effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ import Menu from '@/app/components/menu';
 import Footer from '@/app/components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@/app/globals.css";
+import Starfield from 'react-starfield';
 // import { FaMaximize } from "react-icons/fa6"; // insert icons like this
 // browse icons here: https://react-icons.github.io/react-icons/
+// todo: learn https://www.remotion.dev/
 
 const orbitron = Orbitron({
     variable: "--font-orbitron",
@@ -29,6 +31,13 @@ export default function RootLayout({
                 <Header />
                 <Menu />
                 {children}
+                <Footer />
+                <Starfield
+                    starCount={2000}
+                    starColor={[255, 255, 255]}
+                    speedFactor={0.03}
+                    backgroundColor="black"
+                />
                 <div className='time-vortex'>
                     <div className='spiral spiral1'></div>
                     <div className='spiral spiral2'></div>
@@ -39,7 +48,6 @@ export default function RootLayout({
                     <div className='spiral spiral7'></div>
                     <div className='spiral spiral8'></div>
                 </div>
-                <Footer />
             </body>
         </html>
     );
