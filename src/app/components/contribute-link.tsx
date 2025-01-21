ff148eceaa04ffd5eb450549892994708b551793
jameson <jamesonmacarthur@gmail.com>
2025-01-21 09:56:48 -0700

    fixes build errors around escaping single quotes

--- src/app/components/contribute-link.tsx
+++ src/app/components/contribute-link.tsx
@@ -29,11 +29,16 @@ export default function ContributeLink() {
                 </Modal.Header>
                 <Modal.Body>
                     <p>Time Travel Television is always looking for great new videos!</p>
-                    <p>Send us a YouTube link to a video you'd like to see added. We'll review and consider adding it! <a href="mailto:suggestions@timetraveltelevision.com">suggestions@timetraveltelevision.com</a></p>
+                    <p>Send us a YouTube link to a video you&rsquo;d like to see added. We&rsquo;ll review and consider adding it! <a href="mailto:suggestions@timetraveltelevision.com">suggestions@timetraveltelevision.com</a></p>
                     <hr/>
                     <h4>Just want to say thanks?</h4>
                     <p>Help us keep the lights on by clicking the button below to send us a couple bucks.</p>
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_s-xclick"/><input type="hidden" name="hosted_button_id" value="W4W36HRRTZFMG"/><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" style={{ border: 'none' }} name="submit" alt="PayPal - The safer, easier way to pay online!"/><img alt="" style={{ border: 'none' }} src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/></form>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                        <input type="hidden" name="cmd" value="_s-xclick"/>
+                        <input type="hidden" name="hosted_button_id" value="W4W36HRRTZFMG"/>
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" style={{ border: 'none' }} name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+                        <img alt="" style={{ border: 'none' }} src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+                    </form>
                 </Modal.Body>
                 {/* <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
