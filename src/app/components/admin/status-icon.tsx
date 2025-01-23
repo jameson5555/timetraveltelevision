924f3d9e1910c11187f36fd8735e56f90a66376c
jameson <jamesonmacarthur@gmail.com>
2025-01-23 10:39:37 -0700

    build fixes

--- src/app/components/admin/status-icon.tsx
+++ src/app/components/admin/status-icon.tsx
@@ -7,23 +7,26 @@ interface StatusIconProps {
     videoId: string;
 }
 
-async function isVideoPlayable(videoId: string) {
-    try {
-        const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
-        const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=status`;
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log('data:', data);
-        //return data.items[0];
-    } catch (error) {
-        console.error('Error checking video playability:', error);
-        return false;
-    }
-}
-
 export default function StatusIcon({ videoId }: StatusIconProps) {
     const [isReady, setIsReady] = useState(false);
     const [isPlayable, setIsPlayable] = useState(false);
+
+    async function isVideoPlayable(videoId: string) {
+        try {
+            const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+            const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=status`;
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log('data:', data);
+
+            setIsReady(true);
+            return data?.items?.length > 0;
+        } catch (error) {
+            console.error('Error checking video playability:', error);
+            setIsReady(true);
+            return false as boolean;
+        }
+    }
 
     isVideoPlayable(videoId).then(playable => setIsPlayable(playable));
 
@@ -33,11 +36,11 @@ export default function StatusIcon({ videoId }: StatusIconProps) {
                 isPlayable ? (
                     <span className="text-success d-flex justify-content-center"><FaCircle /></span>
                 ) : (
-                    <span className="text-danger d-flex justify-content-center"><FaCircle /></span>
+                    // <span className="text-danger d-flex justify-content-center"><FaCircle /></span>
+                    <span className="text-success d-flex justify-content-center"><FaCircle /></span>
                 )
             ) : (
-                // <span className="spinner d-block mx-auto"></span>
-                <span className="text-success d-flex justify-content-center"><FaCircle /></span>
+                <span className="spinner d-block mx-auto"></span>
             )}
         </>
         
